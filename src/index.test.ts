4258e11305939f63f84371f6857a483c01d01afb
Brent Mealhouse <bmealhouse@gmail.com>
2021-01-10 21:55:57 -0600

    Upgrade dependencies and switch to tsdx

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,175 +1,174 @@
-import * as fs from 'fs';
-import axios from 'axios';
-import useAxiosRecordReplayAdapter from '.';
+import * as fs from "fs";
+import axios from "axios";
+import useAxiosRecordReplayAdapter from ".";
 
 const format = (data: string | object): string => {
-	if (typeof data !== 'string') {
-		data = JSON.stringify(data, null, 2);
-	}
+  if (typeof data !== "string") {
+    data = JSON.stringify(data, null, 2);
+  }
 
-	return data.replace(/"/g, '’');
+  return data.replace(/"/g, "’");
 };
 
 afterEach(() => {
-	try {
-		const recordings = fs
-			.readdirSync('./recordings')
-			.map(filename => `./recordings/${filename}`);
+  try {
+    const recordings = fs
+      .readdirSync("./recordings")
+      .map(filename => `./recordings/${filename}`);
 
-		recordings.forEach(fs.unlinkSync);
-		fs.rmdirSync('./recordings');
-	} catch {}
+    recordings.forEach(fs.unlinkSync);
+    fs.rmdirSync("./recordings");
+  } catch {}
 });
 
-test('creates the default recordings directory', () => {
-	useAxiosRecordReplayAdapter({
-		axiosInstance: axios.create()
-	});
-	expect(fs.existsSync('./recordings')).toBeTruthy();
+test("creates the default recordings directory", () => {
+  useAxiosRecordReplayAdapter({
+    axiosInstance: axios.create(),
+  });
+  expect(fs.existsSync("./recordings")).toBeTruthy();
 });
 
-test('creates a custom recordings directory', () => {
-	const recordingsDir = './src/recordings';
+test("creates a custom recordings directory", () => {
+  const recordingsDir = "./src/recordings";
 
-	useAxiosRecordReplayAdapter({
-		axiosInstance: axios.create(),
-		recordingsDir
-	});
+  useAxiosRecordReplayAdapter({
+    axiosInstance: axios.create(),
+    recordingsDir,
+  });
 
-	expect(fs.existsSync(recordingsDir)).toBeTruthy();
-	fs.rmdirSync(recordingsDir);
+  expect(fs.existsSync(recordingsDir)).toBeTruthy();
+  fs.rmdirSync(recordingsDir);
 });
 
-test('creates a recording with default filename', async () => {
-	const axiosInstance = axios.create();
-	useAxiosRecordReplayAdapter({axiosInstance});
+test("creates a recording with default filename", async () => {
+  const axiosInstance = axios.create();
+  useAxiosRecordReplayAdapter({ axiosInstance });
 
-	const {data} = await axiosInstance.get('https://reqres.in/api/users/7');
+  const { data } = await axiosInstance.get("https://reqres.in/api/users/7");
 
-	const [recording] = fs.readdirSync('./recordings');
-	expect(recording).toMatchInlineSnapshot(
-		`"1ab499d565acf87f67ac738631d42d07.json"`
-	);
+  const [recording] = fs.readdirSync("./recordings");
+  expect(recording).toMatchInlineSnapshot(
+    `"1ab499d565acf87f67ac738631d42d07.json"`
+  );
 
-	const recordingContents = fs.readFileSync(
-		`./recordings/${recording}`,
-		'utf8'
-	);
-	expect(JSON.parse(recordingContents).response.data).toEqual(data);
+  const recordingContents = fs.readFileSync(
+    `./recordings/${recording}`,
+    "utf8"
+  );
+  expect(JSON.parse(recordingContents).response.data).toEqual(data);
 
-	expect(format(recordingContents)).toMatchInlineSnapshot(`
-		"{
-		  ’request’: {
-		    ’method’: ’get’,
-		    ’path’: ’/api/users/7’
-		  },
-		  ’response’: {
-		    ’status’: 200,
-		    ’statusText’: ’OK’,
-		    ’data’: {
-		      ’data’: {
-		        ’id’: 7,
-		        ’email’: ’michael.lawson@reqres.in’,
-		        ’first_name’: ’Michael’,
-		        ’last_name’: ’Lawson’,
-		        ’avatar’: ’https://s3.amazonaws.com/uifaces/faces/twitter/follettkyle/128.jpg’
-		      },
-		      ’ad’: {
-		        ’company’: ’StatusCode Weekly’,
-		        ’url’: ’http://statuscode.org/’,
-		        ’text’: ’A weekly newsletter focusing on software development, infrastructure, the server, performance, and the stack end of things.’
-		      }
-		    }
-		  }
-		}"
-	`);
+  expect(format(recordingContents)).toMatchInlineSnapshot(`
+    "{
+      ’request’: {
+        ’method’: ’get’,
+        ’path’: ’/api/users/7’
+      },
+      ’response’: {
+        ’status’: 200,
+        ’statusText’: ’OK’,
+        ’data’: {
+          ’data’: {
+            ’id’: 7,
+            ’email’: ’michael.lawson@reqres.in’,
+            ’first_name’: ’Michael’,
+            ’last_name’: ’Lawson’,
+            ’avatar’: ’https://reqres.in/img/faces/7-image.jpg’
+          },
+          ’support’: {
+            ’url’: ’https://reqres.in/#support-heading’,
+            ’text’: ’To keep ReqRes free, contributions towards server costs are appreciated!’
+          }
+        }
+      }
+    }"
+  `);
 });
 
-test('creates a recording with filename prefix', async () => {
-	const axiosInstance = axios.create();
+test("creates a recording with filename prefix", async () => {
+  const axiosInstance = axios.create();
 
-	useAxiosRecordReplayAdapter({
-		axiosInstance,
-		buildFilenamePrefix(request) {
-			return request.path.replace(/\//g, '-').slice(1);
-		}
-	});
+  useAxiosRecordReplayAdapter({
+    axiosInstance,
+    buildFilenamePrefix(request) {
+      return request.path.replace(/\//g, "-").slice(1);
+    },
+  });
 
-	await axiosInstance.get('https://reqres.in/api/users/7');
+  await axiosInstance.get("https://reqres.in/api/users/7");
 
-	const [recording] = fs.readdirSync('./recordings');
-	expect(recording).toMatchInlineSnapshot(
-		`"api-users-7_1ab499d565acf87f67ac738631d42d07.json"`
-	);
+  const [recording] = fs.readdirSync("./recordings");
+  expect(recording).toMatchInlineSnapshot(
+    `"api-users-7_1ab499d565acf87f67ac738631d42d07.json"`
+  );
 });
 
-test('returns a cached response when a recording exists', async () => {
-	const axiosInstance = axios.create({
-		baseURL: 'https://reqres.in'
-	});
+test("returns a cached response when a recording exists", async () => {
+  const axiosInstance = axios.create({
+    baseURL: "https://reqres.in",
+  });
 
-	const spy = jest.spyOn(axiosInstance.defaults, 'adapter');
+  const spy = jest.spyOn(axiosInstance.defaults, "adapter");
 
-	useAxiosRecordReplayAdapter({axiosInstance});
-	await axiosInstance.get('/api/users/7');
-	await axiosInstance.get('/api/users/7');
-	await axiosInstance.get('/api/users/7');
+  useAxiosRecordReplayAdapter({ axiosInstance });
+  await axiosInstance.get("/api/users/7");
+  await axiosInstance.get("/api/users/7");
+  await axiosInstance.get("/api/users/7");
 
-	expect(spy).toHaveBeenCalledTimes(1);
+  expect(spy).toHaveBeenCalledTimes(1);
 });
 
-test('records a custom request', async () => {
-	const axiosInstance = axios.create();
+test("records a custom request", async () => {
+  const axiosInstance = axios.create();
 
-	useAxiosRecordReplayAdapter({
-		axiosInstance,
-		buildRequest(requestConfig) {
-			return {path: new URL(requestConfig.url!).pathname};
-		}
-	});
+  useAxiosRecordReplayAdapter({
+    axiosInstance,
+    buildRequest(requestConfig) {
+      return { path: new URL(requestConfig.url!).pathname };
+    },
+  });
 
-	await axiosInstance.get('https://reqres.in/api/users/7');
-	const [recording] = fs.readdirSync('./recordings');
-	const {request} = JSON.parse(
-		fs.readFileSync(`./recordings/${recording}`, 'utf8')
-	);
+  await axiosInstance.get("https://reqres.in/api/users/7");
+  const [recording] = fs.readdirSync("./recordings");
+  const { request } = JSON.parse(
+    fs.readFileSync(`./recordings/${recording}`, "utf8")
+  );
 
-	expect(format(request)).toMatchInlineSnapshot(`
-		"{
-		  ’path’: ’/api/users/7’
-		}"
-	`);
+  expect(format(request)).toMatchInlineSnapshot(`
+    		"{
+    		  ’path’: ’/api/users/7’
+    		}"
+  	`);
 });
 
-test('create a custom reponse', async () => {
-	const axiosInstance = axios.create();
+test("create a custom reponse", async () => {
+  const axiosInstance = axios.create();
 
-	useAxiosRecordReplayAdapter({
-		axiosInstance,
-		buildResponse() {
-			return {data: Buffer.from(new ArrayBuffer(128))};
-		}
-	});
+  useAxiosRecordReplayAdapter({
+    axiosInstance,
+    buildResponse() {
+      return { data: Buffer.from(new ArrayBuffer(128)) };
+    },
+  });
 
-	await axiosInstance.get('https://reqres.in/api/users/7');
-	const [recording] = fs.readdirSync('./recordings');
-	const {response} = JSON.parse(
-		fs.readFileSync(`./recordings/${recording}`, 'utf8')
-	);
+  await axiosInstance.get("https://reqres.in/api/users/7");
+  const [recording] = fs.readdirSync("./recordings");
+  const { response } = JSON.parse(
+    fs.readFileSync(`./recordings/${recording}`, "utf8")
+  );
 
-	expect(format(response)).toMatchInlineSnapshot(`
-		"{
-		  ’data’: ’[Buffer]’
-		}"
-	`);
+  expect(format(response)).toMatchInlineSnapshot(`
+    		"{
+    		  ’data’: ’[Buffer]’
+    		}"
+  	`);
 });
 
-test('restores default axios adapter', () => {
-	const defaultAdapter = axios.defaults.adapter;
+test("restores default axios adapter", () => {
+  const defaultAdapter = axios.defaults.adapter;
 
-	const restoreDefaultAdapater = useAxiosRecordReplayAdapter();
-	expect(axios.defaults.adapter).not.toBe(defaultAdapter);
+  const restoreDefaultAdapater = useAxiosRecordReplayAdapter();
+  expect(axios.defaults.adapter).not.toBe(defaultAdapter);
 
-	restoreDefaultAdapater();
-	expect(axios.defaults.adapter).toBe(defaultAdapter);
+  restoreDefaultAdapater();
+  expect(axios.defaults.adapter).toBe(defaultAdapter);
 });
